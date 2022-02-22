701ad0d1a109c6557f97f612f4db2d57c2a8bc79
etmula <mountainroad@hotmail.co.jp>
2022-02-23 00:20:26 +0900

    Update DEBUG

--- bona_blog/settings.py
+++ bona_blog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h((g0d1e)y#zn%d!2wt_ow5otovu5nd#lv7v#_h_o&(m$vtbzc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = ['127.0.0.1', 'bona-blog.herokuapp.com']
 
